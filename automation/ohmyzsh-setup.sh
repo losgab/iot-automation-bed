610d4ecf39225001fdcb6d4a2bdbe3c04da4a9a3
Gabriel Thien <53280648+losgab@users.noreply.github.com>
2023-12-27 14:24:34 +1100

    added oh my zsh setup scripts

---
+++ automation/ohmyzsh-setup.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+curl https://raw.githubusercontent.com/losgab/general/main/automation/.zshrc > .zshrc
