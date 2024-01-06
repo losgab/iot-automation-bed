dac0788af136c3fcad66c89f6f22d7fc3894259e
Gabriel Thien <53280648+losgab@users.noreply.github.com>
2024-01-06 20:05:19 +1100

    zsh prototyping

--- automation/ohmyzsh-setup.sh
+++ automation/ohmyzsh-setup.sh
@@ -1,4 +1,3 @@
+curl https://raw.githubusercontent.com/losgab/iot-automation-bed/main/automation/.zshrc > .zshrc
+exec zsh -l
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-curl https://raw.githubusercontent.com/losgab/general/main/automation/.zshrc > .zshrc
